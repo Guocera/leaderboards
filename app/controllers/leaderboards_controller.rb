5545cb5e290d903a373a4134f289b7c59703aef3
Nick Yee <njsyee@gmail.com>
2016-04-19 06:07:30 -0600

    Created Person model.  Added table listing all people

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -1,6 +1,12 @@
 class LeaderboardsController < ApplicationController
   def index
-    
+    response = token.get('/api/v1/people', headers: { "Accept": "application/json" }, params: { page: 1 } )
+    @people = JSON.parse(response.body)["results"]
+    @people.each do |person|
+      puts "First name: ", person['first_name']
+      puts "Employer: ", person['employer']
+      puts
+    end
   end
 
 end
