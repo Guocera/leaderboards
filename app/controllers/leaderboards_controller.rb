cdf04854f49f900484855a51e7471b4c8935aac4
Nick Yee <njsyee@gmail.com>
2016-04-19 06:12:00 -0600

    Leaderboards only shows people who have the program tag

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -1,12 +1,7 @@
 class LeaderboardsController < ApplicationController
   def index
-    response = token.get('/api/v1/people', headers: { "Accept": "application/json" }, params: { page: 1 } )
+    response = token.get("/api/v1/tags/#{program_tag}/people", headers: { "Accept": "application/json" })
     @people = JSON.parse(response.body)["results"]
-    @people.each do |person|
-      puts "First name: ", person['first_name']
-      puts "Employer: ", person['employer']
-      puts
-    end
   end
 
 end
