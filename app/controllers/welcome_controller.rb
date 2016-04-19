10262c34e8a4a93e526b6186827ba23027d4d7b7
Nick Yee <njsyee@gmail.com>
2016-04-19 11:19:10 -0600

    Fixed local and production oauth

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     redirect_uri = get_redirect_url
+    puts "redirect_uri", redirect_uri
     client = get_client
     @oauth_authorize = client.auth_code.authorize_url(redirect_uri: redirect_uri)    
   end
