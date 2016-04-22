20b2c6b3fe3b0ff44bb0073a755071e9fad1420e
Nick Yee <njsyee@gmail.com>
2016-04-22 06:44:08 -0600

    Removed debugging puts

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     redirect_uri = get_redirect_url
-    puts "redirect_uri", redirect_uri
     client = get_client
     @oauth_authorize = client.auth_code.authorize_url(redirect_uri: redirect_uri)    
   end
