01f2c14bcdeb65ef997cd0f10b53ab11db8d2cca
Nick Yee <njsyee@gmail.com>
2016-04-25 06:27:53 -0600

    Individiual boards show employees table

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -40,7 +40,20 @@ class LeaderboardsController < ApplicationController
   end
 
   def show
-    
+    response = token.get("/api/v1/tags/#{program_tag}-#{params[:id]}/people", headers: { "Accept": "application/json" })
+    @people = JSON.parse(response.body)["results"]
+
+    @people.each_with_index do |person, i|
+      id = person['id']
+  
+      response = token.get("/api/v1/people/#{id}/capitals", headers: { "Accept": "application/json" })
+      capitals = JSON.parse(response.body)["results"]
+
+      @people[i]['capital_amount_in_cents'] = 0
+      capitals.each do |capital|
+        @people[i]['capital_amount_in_cents'] += (capital['amount_in_cents'].to_f / 100.0).to_i
+      end
+    end
   end
 
 end
